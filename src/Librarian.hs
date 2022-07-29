4e57cd067722570b3946612c76b19cca5e59f0bd
Gautier DI FOLCO <gautier.difolco@gmail.com>
2022-07-29 20:21:14 +0200

    Build the code logic

--- src/Librarian.hs
+++ src/Librarian.hs
@@ -1,4 +1,139 @@
 module Librarian
-  (
+  ( Rule (..),
+    RuleName (..),
+
+    -- * Collecting
+    Matcher (..),
+    Mover (..),
+    CollectedFiles,
+    fetchRulesOn,
+
+    -- * Planning
+    Move (..),
+    planMoves,
+    displayPlan,
+
+    -- * Runner
+    Action (..),
+    ActionResult (..),
+    RunResult,
+    runPlan,
+    displayResult,
   )
 where
+
+import Control.Exception (catch)
+import Control.Monad
+import Data.Function (on)
+import Data.Functor (($>))
+import Data.List (find, nubBy, sortOn)
+import qualified Data.Map.Strict as Map
+import Data.Maybe (catMaybes, fromMaybe, isJust)
+import GHC.Exts (IsString)
+import GHC.Generics (Generic)
+import System.Directory (createDirectoryIfMissing, doesFileExist, renameFile)
+import System.EasyFile (splitFileName)
+import System.FilePath.Glob (compile, globDir)
+import Text.RegexPR
+import Text.Show.Pretty
+
+data Rule = Rule
+  { name :: RuleName,
+    match :: Matcher,
+    movers :: [Mover]
+  }
+  deriving stock (Eq, Show, Generic)
+
+newtype RuleName = RuleName String
+  deriving stock (Generic)
+  deriving newtype (Eq, Ord, Show, IsString)
+
+newtype Matcher = Matcher {matchPattern :: String}
+  deriving stock (Generic)
+  deriving newtype (Eq, Ord, Show, IsString)
+
+data Mover = Mover
+  { inputPattern :: String,
+    newName :: String
+  }
+  deriving stock (Eq, Show, Generic)
+
+type CollectedFiles = Map.Map FilePath Rule
+
+fetchRulesOn :: FilePath -> [Rule] -> IO CollectedFiles
+fetchRulesOn root rules = do
+  matches <- globDir (compile . matchPattern . match <$> rules) root
+  let distributeRule :: [FilePath] -> Rule -> [(FilePath, Rule)]
+      distributeRule fs r = map (\f -> (f, r)) fs
+  files <- mapM (filterM doesFileExist) matches
+  return $ Map.unions $ map Map.fromList $ zipWith distributeRule files rules
+
+data Move = Move
+  { original :: FilePath,
+    new :: Maybe FilePath,
+    rule :: Rule
+  }
+  deriving stock (Eq, Show, Generic)
+
+planMoves :: CollectedFiles -> [Move]
+planMoves = map (uncurry planMove) . Map.toList
+  where
+    planMove :: FilePath -> Rule -> Move
+    planMove p rule =
+      Move
+        { original = p,
+          new = newPath p $ movers rule,
+          rule = rule
+        }
+    newPath :: FilePath -> [Mover] -> Maybe FilePath
+    newPath p =
+      fmap (\mover -> subRegexPR (inputPattern mover) (newName mover) p)
+        . find (isJust . flip matchRegexPR p . inputPattern)
+
+displayPlan :: [Move] -> IO ()
+displayPlan xs = do
+  forM_ xs $ \Move {..} ->
+    putStrLn $ "[" <> show (name rule) <> "] '" <> original <> "' -> '" <> fromMaybe "UNABLE TO REPLACE" new <> "'"
+  let unrenamed = nubBy ((==) `on` name) $ sortOn name $ map rule $ filter (isJust . new) xs
+  unless (null unrenamed) $ do
+    putStrLn "Unable to generate a new name for:"
+    forM_ unrenamed pPrint
+
+data Action = Action
+  { from :: FilePath,
+    to :: FilePath
+  }
+  deriving stock (Eq, Show, Generic)
+
+data ActionResult
+  = Done
+  | Existing
+  | IOException IOError
+  deriving stock (Eq, Show, Generic)
+
+type RunResult = [(Action, ActionResult)]
+
+runPlan :: [Move] -> IO RunResult
+runPlan = fmap catMaybes . run
+  where
+    run =
+      mapM $ \Move {..} ->
+        case new of
+          Nothing -> return Nothing
+          Just newPath -> do
+            let action = Action original newPath
+            existing <- doesFileExist newPath
+            if existing
+              then return $ Just (action, Existing)
+              else do
+                prepareDirectory newPath
+                Just . (,) action <$> ((renameFile original newPath $> Done) `catch` (return . IOException))
+    prepareDirectory = createDirectoryIfMissing True . fst . splitFileName
+
+displayResult :: RunResult -> IO ()
+displayResult =
+  mapM_ $ \(Action {..}, result) ->
+    case result of
+      Done -> return ()
+      Existing -> putStrLn $ "'" <> from <> "' -> '" <> to <> "' ALREADY EXISTING"
+      IOException e -> putStrLn $ "'" <> from <> "' -> '" <> to <> "' IOError (" <> show e <> ")"
--- test/LibrarianSpec.hs
+++ test/LibrarianSpec.hs
@@ -4,7 +4,11 @@ module LibrarianSpec
   )
 where
 
-import Librarian()
+import qualified Data.Map.Strict as Map
+import Librarian
+import System.Directory
+import System.EasyFile
+import System.IO.Temp
 import Test.Hspec
 
 main :: IO ()
@@ -12,5 +16,67 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  it "failing test" $
-    True `shouldBe` True
+  describe "fetchRulesOn" $ do
+    it "Empty target directory should be empty" $
+      withFiles [] (fetchRulesOn "." rules)
+        `shouldReturn` mempty
+    it "Text files only should match only all rule" $
+      withFiles ["in/sub/0.txt", "in/1.txt"] (fetchRulesOn "." rules)
+        `shouldReturn` Map.fromList [("./in/sub/0.txt", rule1All), ("./in/1.txt", rule1All)]
+    it "Text/images files should match by priority" $
+      withFiles ["in/sub/0.jpg", "in/1.txt"] (fetchRulesOn "." rules)
+        `shouldReturn` Map.fromList [("./in/sub/0.jpg", rule0Jpg), ("./in/1.txt", rule1All)]
+  describe "planMoves" $ do
+    it "Images should be moved, texts should have their extension changed" $
+      planMoves (Map.fromList [("./in/sub/0.jpg", rule0Jpg), ("./in/1.txt", rule1All)])
+        `shouldBe` [ Move "./in/1.txt" (Just "./in/1.TXT") rule1All,
+                     Move "./in/sub/0.jpg" (Just "out/pics/0.jpg") rule0Jpg
+                   ]
+    it "Non-matching mover should be nothing" $
+      planMoves (Map.fromList [("./in/1.png", rule1All)])
+        `shouldBe` [Move "./in/1.png" Nothing rule1All]
+  describe "runPlan" $ do
+    it "Overriding path should block" $
+      withFiles ["in/0.txt", "in/sub/0.txt"] (fetchRulesOn "." [overridingRule] >>= runPlan . planMoves)
+        `shouldReturn` [ (Action "./in/0.txt" "out/0.txt", Done),
+                         (Action "./in/sub/0.txt" "out/0.txt", Existing)
+                       ]
+
+withFiles :: [FilePath] -> IO a -> IO a
+withFiles files act =
+  withSystemTempDirectory "librarian-tests" $ \d ->
+    withCurrentDirectory d $ do
+      mapM_ touch files
+      act
+
+touch :: FilePath -> IO ()
+touch target = do
+  createDirectoryIfMissing True $ fst $ splitFileName target
+  writeFile target "-"
+
+rules :: [Rule]
+rules = [rule0Jpg, rule1All]
+
+rule0Jpg :: Rule
+rule0Jpg =
+  Rule
+    { name = "Image files",
+      match = "**/*.jpg",
+      movers = [Mover "^.*/([^\\/]+)$" "out/pics/\\1"]
+    }
+
+rule1All :: Rule
+rule1All =
+  Rule
+    { name = "All files",
+      match = "**/*",
+      movers = [Mover "pdf$" "PDF", Mover "txt$" "TXT", Mover "txt$" "TxT"]
+    }
+
+overridingRule :: Rule
+overridingRule =
+  Rule
+    { name = "Text files",
+      match = "**/*.txt",
+      movers = [Mover "^.*/([^\\/]+)$" "out/\\1"]
+    }
